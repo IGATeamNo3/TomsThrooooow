4aaafe5c96c56ffb956477497338b16e52abbf31
solomonlu <52169479@qq.com>
2017-07-24 11:17:37 +0800

    throwable actor crush effect

--- Source/TomsThrooooow/Item/ThrowableActor.h
+++ Source/TomsThrooooow/Item/ThrowableActor.h
@@ -26,4 +26,15 @@ public:
 protected:
 	UPROPERTY(Category = ThrowableActor, VisibleAnywhere, BlueprintReadOnly, meta = (ExposeFunctionCategories = "Mesh,Rendering,Physics,Components|StaticMesh", AllowPrivateAccess = "true"))
 	class UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(Category = "ThrowableActor | Crush", EditAnywhere)
+	bool bCrushableOnHit;
+
+	UPROPERTY(Category = "ThrowableActor | Crush", EditAnywhere, meta = (EditCondition = "bCrushableOnHit"))
+	float CrushThreshold;
+
+	UPROPERTY(Category = "ThrowableActor | Crush", EditAnywhere , meta = (EditCondition = "bCrushableOnHit"))
+	class UParticleSystem* CrushEffect;
+
+
 };
